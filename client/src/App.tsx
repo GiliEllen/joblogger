46d5b0c35632c82f19f19594a8f4785e83390cc8
Gili <gilimena95@gmail.com>
2023-06-19 14:36:57 +0300

    login to the side is complete with mui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 import RegisterForm from "./views/Register";
 import LoginForm from "./views/Login";
 import Home from "./views/home/Home";
+import SignInSide from "./views/LoginToTheSide";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginForm/>} />
+        <Route path="/" element={<SignInSide/>} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/home" element={<Home />} />
 
