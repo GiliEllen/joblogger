89c281d4a052b7e6a26f08cee801de41d8a1acc3
Gili <gilimena95@gmail.com>
2023-07-02 13:06:49 +0300

    menu done and logout

--- client/src/components/DrawerMenue.tsx
+++ client/src/components/DrawerMenue.tsx
@@ -7,9 +7,18 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
+import axios from "axios";
+import { useAppDispatch } from "../app/hooks";
+import { logout } from "../features/user/userSlice";
 
 const DrawerMenue = () => {
   const [open, setOpen] = useState(false);
+  const dispatch = useAppDispatch()
+
+  const handleLogout = async () => {
+    const {data} = await axios.get("/api/users/logout");
+    dispatch(logout())
+  }
 
   return (
     <Grid container justifyContent="flex-start" alignItems="center">
@@ -23,22 +32,22 @@ const DrawerMenue = () => {
           <List>
             <ListItem button onClick={() => setOpen(false)}>
               <ListItemText>
-                <Link style={{ textDecoration: "none" }} to="/">
+                <Link style={{ textDecoration: "none" }} to="/home">
                   Home
                 </Link>
               </ListItemText>
             </ListItem>
             <ListItem button onClick={() => setOpen(false)}>
               <ListItemText>
-                <Link style={{ textDecoration: "none" }} to="/about">
-                  About Us
+                <Link style={{ textDecoration: "none" }} to="/add-job">
+                  Add Job
                 </Link>
               </ListItemText>
             </ListItem>
             <ListItem button onClick={() => setOpen(false)}>
               <ListItemText>
-                <Link style={{ textDecoration: "none" }} to="/contact">
-                  Contact Us
+                <Link onClick={handleLogout} style={{ textDecoration: "none" }} to="/">
+                  LogOut
                 </Link>
               </ListItemText>
             </ListItem>
